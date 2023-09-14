56985674de5b1350ebaf0789670546b28e43c3b6
RYAN VALDIVIA <rvaldiviase@unsa.edu.pe>
2023-09-14 11:37:45 -0500

    Mejorando el programa, ahora escribe en el archivo 'notas.txt' la asignatura, nombre del alumno y la respectiva calificación final

--- fase00/Notas.java
+++ fase00/Notas.java
@@ -5,15 +5,25 @@ public class Notas {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         FileWriter fw = new FileWriter("notas.txt", true);
-        final double[] weigths = new double[] { 0.05, 0.05, 0.15, 0.15, 0.2, 0.4 };
         for (int i = 0; i < 100; i++) {
-            System.out.println("Ingrese el nombre del alumno (Ingrese 'q' para cancelar)");
-            String student = sc.next();
-            if (student.equals("q")) {
+            System.out.println("¿Nuevo cálculo? (y/n)");
+
+            String rpta = sc.nextLine();
+            if (rpta.equals("n")) {
                 break;
             }
-            fw.write(student);
-            fw.write(" ");
+            System.out.println("Ingrese el nombre de la asignatura");
+            String subject = sc.nextLine();
+
+            System.out.println("Ingrese el nombre del alumno");
+            String student = sc.next();
+
+            System.out.println("Ingrese los ponderados");
+            double[] weigths = new double[6];
+            for (int j = 0; j < 6; j++) {
+                weigths[j] = sc.nextDouble();
+            }
+
             System.out.println("Ingrese las notas del alumno");
             double avg = 0;
             for (int j = 0; j < 6; j++) {
@@ -21,7 +31,10 @@ public class Notas {
                 grades[j] = sc.nextDouble();
                 avg += grades[j] * weigths[j];
             }
-            fw.write("El promedio final es: " + avg);
+            String basura = sc.nextLine();
+
+            fw.write("Hola " + student + ", tu promedio en la asignatura " + subject + " es: " + avg);
+            fw.write("\r\n");
             fw.close();
         }
     }
