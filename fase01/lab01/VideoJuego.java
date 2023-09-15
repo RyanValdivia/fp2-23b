bff63212c5305adb7e22ff76e1379c78abbbb8da
RYAN VALDIVIA <rvaldiviase@unsa.edu.pe>
2023-09-15 10:09:26 -0500

    Actividad 5: Creando el código para inicializar dos ejércitos y ver cuál de ellos es el ganador usando arreglos

--- fase01/lab01/VideoJuego.java
+++ fase01/lab01/VideoJuego.java
@@ -2,22 +2,44 @@ import java.util.*;
 
 public class VideoJuego {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        int len1 = (int) (Math.random() * 5 + 1);
+        int len2 = (int) (Math.random() * 5 + 1);
+        String[] army1 = new String[len1];
+        String[] army2 = new String[len2];
+        init(army1);
+        init(army2);
+        mostrar(army1, army2);
+        win(len1, len2);
 
-        System.out.println("Ingrese los nombres de los soldados y sus respectivos niveles de vida");
+    }
 
-        String[] soldiers = new String[5];
-        int[] levels = new int[5];
-
-        for (int i = 0; i < 5; i++) {
-            soldiers[i] = sc.next();
-            levels[i] = sc.nextInt();
+    public static void init(String[] strs) {
+        for (int i = 0; i < strs.length; i++) {
+            strs[i] = "Soldado" + i;
         }
+    }
 
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Soldado " + i + ": " + soldiers[i] + " y su nivel de vida es: " + levels[i]);
+    public static void win(int l1, int l2) {
+        if (l1 > l2) {
+            System.out.println("El ejército 1 es ganador");
+        } else if (l1 == l2) {
+            System.out.println("Hay empate");
+        } else {
+            System.out.println("El ejército 2 es ganador");
         }
+    }
 
-        sc.close();
+    public static void mostrar(String[] strs1, String[] strs2) {
+        System.out.println("Ejercito 1: ");
+        for (String s : strs1) {
+            System.out.println(s);
+        }
+        System.out.println();
+
+        System.out.println("Ejercito 2: ");
+        for (String s : strs2) {
+            System.out.println(s);
+        }
+        System.out.println();
     }
 }
