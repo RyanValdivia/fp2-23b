b582a6636f65c7c017eb9ec2562b4783ec9269f9
RYAN VALDIVIA <rvaldiviase@unsa.edu.pe>
2023-09-15 09:42:15 -0500

    Actividad 4: Mejorando el programa para que lea los nombres y niveles de vida de 5 soldados y luego los muestre, usando arreglos

--- fase01/lab01/VideoJuego.java
+++ fase01/lab01/VideoJuego.java
@@ -4,16 +4,18 @@ public class VideoJuego {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Ingrese los nombres de los soldados");
+        System.out.println("Ingrese los nombres de los soldados y sus respectivos niveles de vida");
 
         String[] soldiers = new String[5];
+        int[] levels = new int[5];
 
         for (int i = 0; i < 5; i++) {
             soldiers[i] = sc.next();
+            levels[i] = sc.nextInt();
         }
 
-        for (int i = 1; i < 6; i++) {
-            System.out.println("Soldado " + i + ": " + soldiers[i]);
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Soldado " + i + ": " + soldiers[i] + " y su nivel de vida es: " + levels[i]);
         }
 
         sc.close();
