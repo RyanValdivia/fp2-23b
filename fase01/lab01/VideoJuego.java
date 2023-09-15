13834af8bdf5d879b92ad6a10739401bd6714fea
RYAN VALDIVIA <rvaldiviase@unsa.edu.pe>
2023-09-15 09:25:45 -0500

    Actividad 2: Creando un programa que lea los nombres y niveles de vida de 5 soldados para luego, mostrarlos

--- fase01/lab01/VideoJuego.java
+++ fase01/lab01/VideoJuego.java
@@ -4,18 +4,23 @@ public class VideoJuego {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Ingrese los nombres de los soldados: ");
-        String sold1 = sc.nextLine();
-        String sold2 = sc.nextLine();
-        String sold3 = sc.nextLine();
-        String sold4 = sc.nextLine();
-        String sold5 = sc.nextLine();
+        System.out.println("Ingrese los nombres de los soldados y su respectivo nivel de vida: ");
+        String sold1 = sc.next();
+        int lvl1 = sc.nextInt();
+        String sold2 = sc.next();
+        int lvl2 = sc.nextInt();
+        String sold3 = sc.next();
+        int lvl3 = sc.nextInt();
+        String sold4 = sc.next();
+        int lvl4 = sc.nextInt();
+        String sold5 = sc.next();
+        int lvl5 = sc.nextInt();
 
-        System.out.println("Soldado 1: " + sold1);
-        System.out.println("Soldado 2: " + sold2);
-        System.out.println("Soldado 3: " + sold3);
-        System.out.println("Soldado 4: " + sold4);
-        System.out.println("Soldado 5: " + sold5);
+        System.out.println("Soldado 1: " + sold1 + " y su nivel de vida es: " + lvl1);
+        System.out.println("Soldado 1: " + sold2 + " y su nivel de vida es: " + lvl2);
+        System.out.println("Soldado 1: " + sold3 + " y su nivel de vida es: " + lvl3);
+        System.out.println("Soldado 1: " + sold4 + " y su nivel de vida es: " + lvl4);
+        System.out.println("Soldado 1: " + sold5 + " y su nivel de vida es: " + lvl5);
 
         sc.close();
     }
