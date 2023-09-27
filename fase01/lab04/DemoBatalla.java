c553fff06e9376cc45a2477aafc6738578d2b320
RYAN VALDIVIA <rvaldiviase@unsa.edu.pe>
2023-09-27 17:39:59 -0500

    Añadiendo algunos métodos para poder iniciar con las busquedas binarias, ordenando los arreglos

--- fase01/lab04/DemoBatalla.java
+++ fase01/lab04/DemoBatalla.java
@@ -33,13 +33,28 @@ public class DemoBatalla {
         System.out.println("\nNave con mayor numero de puntos: " + mostrarMayorPuntos(misNaves).getNombre());
         mostrarNaves(desordenar(misNaves));
 
+        String nombre = sc.next();
+        // mostrar los datos de la nave con dicho nombre, mensaje de “no encontrado” en
+        // caso contrario
         int pos = busquedaLinealNombre(misNaves, nombre);
+        if (pos != -1) {
+            Nave n = misNaves[pos];
+            System.out.println("Nave " + pos + ":" + n.getNombre());
+            System.out.println("Posicion: " + n.getFila() + n.getColumna());
+            System.out.println("Puntos: " + n.getPuntos());
+            if (n.getEstado()) {
+                System.out.println("Sigue con vida");
+            } else {
+                System.out.println("Fue destruida");
+            }
+        } else {
+            System.out.println("Nave no encontrada");
+        }
         ordenarPorPuntosBurbuja(misNaves);
         mostrarNaves(misNaves);
         ordenarPorNombreBurbuja(misNaves);
         mostrarNaves(misNaves);
-        // mostrar los datos de la nave con dicho nombre, mensaje de “no encontrado” en
-        // caso contrario
+
         pos = busquedaBinariaNombre(misNaves, nombre);
         ordenarPorPuntosSeleccion(misNaves);
         mostrarNaves(misNaves);
@@ -157,18 +172,52 @@ public class DemoBatalla {
 
     // Método para buscar la primera nave con un nombre que se pidió por teclado
     public static int busquedaLinealNombre(Nave[] flota, String s) {
+        for (int i = 0; i < flota.length; i++) {
+            if (flota[i].getNombre().equals(s)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     // Método que ordena por número de puntos de menor a mayor
     public static void ordenarPorPuntosBurbuja(Nave[] flota) {
+        for (int i = 0; i < flota.length; i++) {
+            for (int j = 0; j < flota.length - 1; j++) {
+                if (flota[j].getPuntos() > flota[j + 1].getPuntos()) {
+                    intercambiar(flota, j, j + 1);
+                }
+            }
+        }
     }
 
     // Método que ordena por nombre de A a Z
     public static void ordenarPorNombreBurbuja(Nave[] flota) {
+        for (int i = 0; i < flota.length; i++) {
+            for (int j = 0; j < flota.length - 1; j++) {
+                if (flota[j].getNombre().compareTo(flota[j + 1].getNombre()) > 0) {
+                    intercambiar(flota, j, j + 1);
+                }
+            }
+        }
     }
 
     // Método para buscar la primera nave con un nombre que se pidió por teclado
     public static int busquedaBinariaNombre(Nave[] flota, String s) {
+        int baja = 0, media, alta = flota.length - 1;
+        while (baja <= alta) {
+            media = (alta + baja) / 2;
+            String nMedio = flota[media].getNombre();
+            int compare = s.compareTo(nMedio);
+            if (compare == 0) {
+                return media;
+            } else if (compare < 0) {
+                alta = media - 1;
+            } else {
+                baja = media + 1;
+            }
+        }
+        return -1;
     }
 
     // Método que ordena por número de puntos de menor a mayor
@@ -185,5 +234,12 @@ public class DemoBatalla {
 
     // Método que muestra las naves ordenadas por nombre de Z a A
     public static void ordenarPorNombreInsercion(Nave[] flota) {
+    }
+
+    public static void intercambiar(Nave[] flota, int i, int j) {
+        Nave temp;
+        temp = flota[i];
+        flota[i] = flota[j];
+        flota[j] = temp;
     }
 }
