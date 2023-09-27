9c6b1af2f2327f5cdbe92071e6cb13a0c5aebe2c
RYAN VALDIVIA <rvaldiviase@unsa.edu.pe>
2023-09-27 00:04:28 -0500

    Creando la clase Soldado para poder utilizar arreglos de objetos para las Actividades 2 y 3

---
+++ fase01/lab03/Soldado.java
@@ -0,0 +1,20 @@
+public class Soldado {
+    private String nombre;
+    private int vida;
+
+    public void setNombre(String s) {
+        this.nombre = s;
+    }
+
+    public void setVida(int n) {
+        this.vida = n;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+}
