66e84446d3ec5c70520c7d3f992fdfead78c58bc
RYAN VALDIVIA <rvaldiviase@unsa.edu.pe>
2023-10-04 11:47:56 -0500

    Implementando el codigo y todos los metodos de busqueda, corrigiendo errores con los metodos de ordenamiento, asegurandonos que el programa sea 100% funcional

--- fase01/prac01/ChistemaUnsa.java
+++ fase01/prac01/ChistemaUnsa.java
@@ -20,42 +20,69 @@ public class ChistemaUnsa {
             chunsa[i].setGenero(Boolean.parseBoolean(sc.next()));
             chunsa[i].setEstado(Boolean.parseBoolean(sc.next()));
         }
-        System.out.println("Bienvenido a la chunsa, que desea buscar?: ");
+        System.out.println("Bienvenido a la chunsa, que desea buscar?");
         System.out.println("Opciones: ");
-        System.out.println("1. Buscar por Nombre");
-        System.out.println("2. Buscar por Apellido");
-        System.out.println("3. Buscar por CUI");
-        System.out.println("4. Buscar por correo");
-        System.out.println("5. Buscar por Fecha de Nacimiento");
+        System.out.println("1. Buscar por Nombre y apellido");
+        System.out.println("2. Buscar por CUI");
+        System.out.println("3. Buscar por correo");
         System.out.println("Ingrese su elección: ");
-        int eleccion = sc.nextInt();
+        int eleccion = sc2.nextInt();
         switch (eleccion) {
             case 1:
+                insertionSortApellido(chunsa);
+                System.out.println("Ingrese el nombre y apellido del estudiante a buscar: ");
+                String nombre = sc2.next(), apellido = sc2.next();
+                int indice = binarySearchNombre(chunsa, nombre, apellido);
+                if (indice == -1) {
+                    System.out.println("Alumno no encontrado");
+                } else {
+                    mostrarAlumno(chunsa, indice);
+                }
+                break;
             case 2:
+                insertionSortCui(chunsa);
+                System.out.println("Ingrese el CUI a buscar: ");
+                int cui = sc2.nextInt();
+                int indice2 = binarySearchCui(chunsa, cui);
+                System.out.println(indice2);
+                if (indice2 == -1) {
+                    System.out.println("Alumno no encontrado");
+                } else {
+                    mostrarAlumno(chunsa, indice2);
+                }
+                break;
             case 3:
-            case 4:
-            case 5:
+                insertionSortCorreo(chunsa);
+                System.out.println("Ingrese el correo institucional a buscar: ");
+                String correo = sc2.next();
+                int indice3 = binarySearchCorreo(chunsa, correo);
+                if (indice3 == -1) {
+                    System.out.println("Alumno no encontrado");
+                } else {
+                    mostrarAlumno(chunsa, indice3);
+                }
+                break;
             default:
         }
 
     }
 
-    public static void insertionSortCui(Estudiante[] chunsa) {
-        for (int i = 0; i < chunsa.length; i++) {
-            Estudiante valor = chunsa[0];
-            int j = i;
-            for (j = i; 0 < j && chunsa[j - 1].getCui() > valor.getCui(); j--) {
-                chunsa[j] = chunsa[j - 1];
-            }
-            chunsa[j] = valor;
-        }
+    public static void mostrarAlumno(Estudiante[] chunsa, int indice) {
+        Estudiante e = chunsa[indice];
+        System.out.println("Nombre completo: " + e.getNombre() + " " + e.getSNombre() + " " + e.getApPaterno() + " "
+                + e.getApMaterno());
+        System.out.println("Correo institucional: " + e.getCorreo());
+        System.out.println("CUI: " + e.getCui());
+        System.out.println("Fecha de nacimiento: " + e.getFecha()[0] + "-" + e.getFecha()[1] + "-" + e.getFecha()[2]);
+        System.out.println("Genero: " + e.getGenero());
+        System.out.println("Estado: " + e.getEstado());
     }
 
-    public static void insertionSortNombre(Estudiante[] chunsa) {
-        for (int i = 1; i < chunsa.length; i++) {
+    public static void insertionSortCui(Estudiante[] chunsa) {
+        for (int i = 0; i < chunsa.length; i++) {
             Estudiante valor = chunsa[i];
             int j = i;
-            for (j = i; 0 < j && chunsa[j - 1].getNombre().compareTo(valor.getNombre()) < 0; j--) {
+            for (j = i; 0 < j && chunsa[j - 1].getCui() > valor.getCui(); j--) {
                 chunsa[j] = chunsa[j - 1];
             }
             chunsa[j] = valor;
@@ -66,7 +93,7 @@ public class ChistemaUnsa {
         for (int i = 1; i < chunsa.length; i++) {
             Estudiante valor = chunsa[i];
             int j = i;
-            for (j = i; 0 < j && chunsa[j - 1].getApPaterno().compareTo(valor.getApPaterno()) < 0; j--) {
+            for (j = i; 0 < j && chunsa[j - 1].getApPaterno().compareTo(valor.getApPaterno()) > 0; j--) {
                 chunsa[j] = chunsa[j - 1];
             }
             chunsa[j] = valor;
@@ -77,20 +104,7 @@ public class ChistemaUnsa {
         for (int i = 1; i < chunsa.length; i++) {
             Estudiante valor = chunsa[i];
             int j = i;
-            for (j = i; 0 < j && chunsa[j - 1].getCorreo().compareTo(valor.getCorreo()) < 0; j--) {
-                chunsa[j] = chunsa[j - 1];
-            }
-            chunsa[j] = valor;
-        }
-    }
-
-    public static void insertionSortFecha(Estudiante[] chunsa) {
-        for (int i = 0; i < chunsa.length; i++) {
-            Estudiante valor = chunsa[0];
-            int j = i;
-            for (j = i; 0 < j && chunsa[j - 1].getFecha()[0] > valor.getFecha()[0]
-                    && chunsa[j - 1].getFecha()[1] > valor.getFecha()[1]
-                    && chunsa[j - 1].getFecha()[2] > valor.getFecha()[2]; j--) {
+            for (j = i; 0 < j && chunsa[j - 1].getCorreo().compareTo(valor.getCorreo()) > 0; j--) {
                 chunsa[j] = chunsa[j - 1];
             }
             chunsa[j] = valor;
@@ -117,7 +131,7 @@ public class ChistemaUnsa {
     public static int binarySearchCui(Estudiante[] chunsa, int cui) {
         int baja = 0, media, alta = chunsa.length - 1;
         while (baja <= alta) {
-            media = (alta + baja) / 2;
+            media = (int) (Math.floor((alta + baja) / 2));
             int valor = chunsa[media].getCui();
             if (valor == cui) {
                 return media;
