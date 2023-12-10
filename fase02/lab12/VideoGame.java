f0b8e1671ee9cd74431c7450ab1b27639c2dcce8
RYAN VALDIVIA <rvaldiviase@unsa.edu.pe>
2023-12-10 16:37:19 -0500

    Añadiendo lo necesario para el menu y el juego personalizado

--- fase02/lab12/VideoGame.java
+++ fase02/lab12/VideoGame.java
@@ -16,6 +16,7 @@ public class VideoGame {
             switch (option) {
                 case 1:
                     /* Juego normal */
+                    int o1;
                     do {
                         System.out.println("Presiona 'q' en cualquier momento para salir");
 
@@ -23,9 +24,9 @@ public class VideoGame {
                         initTable(table);
 
                         ArrayList<Soldier> ej1 = new ArrayList<Soldier>();
-                        initArmy(ej1, 1);
+                        initArmy(ej1, 1, table);
                         ArrayList<Soldier> ej2 = new ArrayList<Soldier>();
-                        initArmy(ej2, 2);
+                        initArmy(ej2, 2, table);
 
                         playArmy(ej1, table);
                         playArmy(ej2, table);
@@ -37,25 +38,118 @@ public class VideoGame {
                         System.out.println("Juego terminado");
                         System.out.println("Presiona 1 para volver al menu principal");
                         System.out.println("Presiona 2 para volver a jugar");
-                    } while (sc.nextInt() == 2);
+                        System.out.println("Presiona 3 para otras opciones: ");
+                        o1 = sc.nextInt();
+                        if (o1 == 3) {
+                        }
+
+                    } while (o1 == 2);
                     break;
                 case 2:
+                    int o2;
                     /* Juego Personalizado */
                     System.out.println("Presiona 'q' en cualquier momento para salir");
-
                     Soldier[][] table = new Soldier[10][10];
                     initTable(table);
 
                     ArrayList<Soldier> ej1 = new ArrayList<Soldier>();
-                    initArmy(ej1, 1);
+                    initArmy(ej1, 1, table);
                     ArrayList<Soldier> ej2 = new ArrayList<Soldier>();
-                    initArmy(ej2, 2);
+                    initArmy(ej2, 2, table);
+
+                    playArmy(ej1, table);
+                    playArmy(ej2, table);
+
+                    showTable(table);
+
+                    do {
+                        System.out.println("Presiona 'q' en cualquier momento para salir");
+
+                        showTable(table);
+
+                        System.out.println("Opciones: ");
+                        System.out.println("1. Crear Soldado: ");
+                        System.out.println("2. Borrar Soldado: ");
+                        System.out.println("3. Clonar Soldado: ");
+                        System.out.println("4. Modificar Soldado: ");
+                        System.out.println("5. Comparar Soldados: ");
+                        System.out.println("6. Intercambiar Soldados: ");
+                        System.out.println("7. Buscar Soldado: ");
+                        System.out.println("8. Ver ejercito: ");
+                        System.out.println("9. Sumar niveles: ");
+                        System.out.println("10. Jugar: ");
+                        System.out.println("11. Salir");
+
+                        o2 = sc.nextInt();
+                        switch (o2) {
+                            case 1:
+                                System.out.println("Seleccione ejercito: ");
+                                if (sc.nextInt() == 1) {
+                                    if (ej1.size() > 10) {
+                                        System.out.println("El ejercito no puede tener mas de 10 soldados");
+                                    } else {
+                                        createSoldier(ej1, 1, table);
+                                        playArmy(ej1, table);
+                                        showTable(table);
+                                    }
+                                } else {
+                                    if (ej2.size() > 10) {
+                                        System.out.println("El ejercito no puede tener mas de 10 soldados");
+                                    } else {
+                                        createSoldier(ej2, 2, table);
+                                        playArmy(ej2, table);
+                                        showTable(table);
+                                    }
+                                }
+                                break;
+                            case 2:
+                                System.out.println("Seleccione ejercito: ");
+                                if (sc.nextInt() == 1) {
+                                    if (ej1.size() == 1) {
+                                        System.out.println("El ejercito no puede estar vacio!");
+                                    } else {
+                                        deleteSoldier(table);
+                                        showTable(table);
+                                    }
+                                } else {
+                                    if (ej2.size() == 1) {
+                                        System.out.println("El ejercito no puede estar vacio!");
+                                    } else {
+                                        deleteSoldier(table);
+                                        showTable(table);
+                                    }
+                                }
+                                break;
+                            case 3:
+                                System.out.println("Seleccione ejercito: ");
+                                if (sc.nextInt() == 1) {
+                                    if (ej1.size() > 10) {
+                                        System.out.println("El ejercito no puede tener mas de 10 soldados");
+                                    } else {
+                                        cloneSoldier(ej1, 1, table);
+                                        playArmy(ej1, table);
+                                    }
+                                } else {
+                                    if (ej2.size() > 10) {
+                                        System.out.println("El ejercito no puede tener mas de 10 soldados");
+                                    } else {
+                                        cloneSoldier(ej2, 2, table);
+                                        playArmy(ej2, table);
+                                    }
+                                }
+                                break;
+                            default:
+                                break;
+                        }
+
+                    } while (o2 != 11);
+
                     break;
                 default:
                     break;
             }
         } while (option != 3);
-
+        sc.close();
     }
 
     public static void initTable(Soldier[][] tb) {
@@ -66,14 +160,11 @@ public class VideoGame {
         }
     }
 
-    public static void initArmy(ArrayList<Soldier> ar, int nro) {
+    public static void initArmy(ArrayList<Soldier> ar, int nro, Soldier[][] tb) {
         int q = (int) (Math.random() * 3) + 1;
         for (int i = 0; i < q; i++) {
             ar.add(new Soldier(i, nro));
         }
-    }
-
-    public static void playArmy(ArrayList<Soldier> ar, Soldier[][] tb) {
         for (int i = 0; i < ar.size(); i++) {
             int x;
             int y;
@@ -82,7 +173,12 @@ public class VideoGame {
                 x = (int) (Math.random() * 10);
             } while (tb[y][x].getStatus());
             ar.get(i).setPosition(y, x);
-            tb[y][x] = ar.get(i);
+        }
+    }
+
+    public static void playArmy(ArrayList<Soldier> ar, Soldier[][] tb) {
+        for (int i = 0; i < ar.size(); i++) {
+            tb[ar.get(i).getRow()][ar.get(i).getColumn()] = ar.get(i);
         }
     }
 
@@ -276,5 +372,76 @@ public class VideoGame {
         System.out.println("Vida: " + s.getcHP() + " / " + s.getMaxHP() + " HP");
         System.out.println("Defensa: " + s.getDefense() + " DP");
         System.out.println("Posicion (x, y): " + s.getColumn() + " - " + s.getRow() + "\n");
+    }
+
+    public static void showArmy(ArrayList<Soldier> s, int id) {
+        System.out.println("Ejercito " + id + "\n");
+        for (int i = 0; i < s.size(); i++) {
+            showSoldier(s.get(i));
+            System.out.println();
+        }
+    }
+
+    public static void createSoldier(ArrayList<Soldier> a, int id, Soldier[][] tb) {
+        Scanner sc = new Scanner(System.in);
+        int atk, hp, def, x, y;
+        System.out.println("Ingrese los datos de su soldado");
+        System.out.println("Ataque: ");
+        atk = sc.nextInt();
+        System.out.println("Vida maxima: ");
+        hp = sc.nextInt();
+        System.out.println("Defensa: ");
+        def = sc.nextInt();
+        System.out.println("Posicion (x, y): ");
+        do {
+            x = sc.nextInt();
+            y = sc.nextInt();
+            if (tb[y][x].getStatus()) {
+                System.out.println("Ya hay un soldado en ese lugar!");
+            }
+        } while (tb[y][x].getStatus());
+
+        Soldier s = new Soldier(atk, def, hp, a.size() - 1, id);
+        s.setPosition(y, x);
+        tb[y][x].copy(s);
+    }
+
+    public static void deleteSoldier(Soldier[][] tb) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Seleccione las coordenadas del soldado a eliminar (x, y)");
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        tb[y][x].die();
+    }
+
+    public static int searchSoldier(ArrayList<Soldier> a, int id) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el nombre del soldado: ");
+        String n = sc.next();
+        for (int i = 0; i < a.size(); i++) {
+            if (n.equals(a.get(i).getName())) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void cloneSoldier(ArrayList<Soldier> a, int id, Soldier[][] tb) {
+        Scanner sc = new Scanner(System.in);
+        int i = searchSoldier(a, id);
+        int x, y;
+        System.out.println("Seleccione las coordenadas a las que se desplegara la copia (x, y): ");
+        do {
+            x = sc.nextInt();
+            y = sc.nextInt();
+            if (tb[y][x].getStatus()) {
+                System.out.println("Ya hay un soldado en ese lugar!");
+            }
+        } while (tb[y][x].getStatus());
+        Soldier s = new Soldier();
+        s.copy(a.get(i));
+        s.setPosition(y, x);
+        s.setName("Soldado" + (a.size() - 1) + "X" + id);
+        a.add(s);
     }
 }
